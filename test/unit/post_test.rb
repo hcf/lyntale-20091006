58a275e5de9ddfa35c855cb758a6471075620fc5
Hans-Christian Fjeldberg <hans-christian@fjeldbergconsulting.no>
2009-11-03 10:34:42 +0100

    Adding failing tests, changed routing, and renamed index

--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -1,8 +1,33 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  def test_post_save
+  
+    post = Post.new
+    
+    post.title = "Testtittel"
+    
+    post.body = "Dette er en test..."
+    
+    post.save
+    
+    assert_not_nil Post.find(post.id)
+    assert_equal "Testtittel", Post.find(post.id).title
+    
   end
+  
+
+  def test_post_must_have_title
+
+    assert_raise(ActiveRecord::RecordInvalid) do
+      post = Post.new
+
+      post.title = ""
+      post.body = "Dette er en test..."
+
+      post.save!
+    end
+  end
+  
 end
